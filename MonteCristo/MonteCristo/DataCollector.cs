78618a0d02a8f30f74cf98d3f19e9f5a42c36603
Paul Dibble <paul.dibble3@gmail.com>
2019-07-22 09:12:41 -0700

    added data colection

--- MonteCristo/MonteCristo/DataCollector.cs
+++ MonteCristo/MonteCristo/DataCollector.cs
@@ -6,5 +6,89 @@ namespace Roulett
 {
     class DataCollector
     {
+
+        private struct dataset
+        {
+            public int rounds;
+           public double initialbet;
+            public double netgain;
+            public string beton;
+            public double initialmoney;
+
+            public dataset(int r, double ib, double ng, string bo, double im)
+            {
+
+                rounds = r;
+                initialbet = ib;
+                netgain = ng;
+                beton = bo;
+                initialmoney = im;
+            }
+        }
+
+        LinkedList<string> betTypesRoulett = new LinkedList<string>();
+        private Player p;
+
+        public DataCollector()
+        {
+            p = new Player();
+            betTypesRoulett.AddFirst("red");
+            betTypesRoulett.AddLast("black");
+            for (int i = 1; i < 37; i++)
+            {
+                betTypesRoulett.AddLast("" + i);
+            }
+        }
+
+        public void RunRoulettStats(int trials,double startingMoney, int maxRounds,double initialBet)
+        {
+            dataset max = new dataset(0,0,0,"",0);
+            LinkedList < dataset > colection = new LinkedList<dataset>();
+
+           
+     
+                foreach (string bettype in betTypesRoulett)
+                {
+                    for (int r = 1; r < maxRounds; r++)
+                    {
+                    double total = 0;
+                    for (int i = 0; i < trials; i++)
+                    {
+                        p.Money = startingMoney;
+                        total+= p.PlayRoulettInput(r, bettype, initialBet);
+                        
+                    }
+                    dataset cur = new dataset(r, initialBet, (total/trials) - startingMoney, bettype, startingMoney);
+                    if (cur.netgain > max.netgain)
+                    {
+                        max = cur;
+                    }
+                    colection.AddLast(cur);
+                   
+
+                }
+                }
+
+            Console.WriteLine("--------Final Results From "+trials+" Trials---------");
+            Console.WriteLine("With starting of $" + startingMoney + " and initial bet of " +max.initialbet);
+            Console.WriteLine("Optimal Rounds: " + max.rounds);
+         //   Console.WriteLine("Optimal initial Bet Amount: " + max.initialbet);
+            Console.WriteLine("Optimal Bet On:" + max.beton);
+            Console.WriteLine("Nate money Made"+ max.netgain);
+
+
+
+            }
+
+
+
+
+
+        
+
+        
+
+
+
     }
 }
--- MonteCristo/MonteCristo/Program.cs
+++ MonteCristo/MonteCristo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace Roulett
 {
@@ -6,15 +7,24 @@ namespace Roulett
     {
         static void Main(string[] args)
         {
-            Player p = new Player(2000);
-            double total = 0;
-            for (int i = 0; i < 10000; i++)
-            {
-                p.Money = 200;
-                total += p.PlayRoulettInput(20, "red", 2);
-            }
-            double avg=total/10000;
-            Console.WriteLine("avg: " + avg);
+
+            Stopwatch sw = new Stopwatch();
+            DataCollector d = new DataCollector();
+            sw.Start();
+            d.RunRoulettStats(10000, 1000, 30, 1);
+            sw.Stop();
+            Console.WriteLine(sw.Elapsed.Seconds+"seconds");
+
+
+            //Player p = new Player(2000);
+            //double total = 0;
+            //for (int i = 0; i < 10000; i++)
+            //{
+            //    p.Money = 200;
+            //    total += p.PlayRoulettInput(20, "red", 2);
+            //}
+            //double avg=total/10000;
+            //Console.WriteLine("avg: " + avg);
 
        
         }
